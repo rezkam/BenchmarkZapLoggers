1575cbb0ac09715577a1b377656e73ea01cebe8d
Reza Kamalifard <mrkamalifard@gmail.com>
2020-07-24 00:19:58 +0430

    log_test: add ztest internal pakcage
    - added to provide methods to define a discarder in logger config

---
+++ internal/ztest/discarder.go
@@ -0,0 +1,34 @@
+package ztest
+
+import "io/ioutil"
+
+// A Syncer is a spy for the Sync portion of zapcore.WriteSyncer.
+type Syncer struct {
+	err    error
+	called bool
+}
+
+// SetError sets the error that the Sync method will return.
+func (s *Syncer) SetError(err error) {
+	s.err = err
+}
+
+// Sync records that it was called, then returns the user-supplied error (if
+// any).
+func (s *Syncer) Sync() error {
+	s.called = true
+	return s.err
+}
+
+// Called reports whether the Sync method was called.
+func (s *Syncer) Called() bool {
+	return s.called
+}
+
+// A Discarder sends all writes to ioutil.Discard.
+type Discarder struct{ Syncer }
+
+// Write implements io.Writer.
+func (d *Discarder) Write(b []byte) (int, error) {
+	return ioutil.Discard.Write(b)
+}
--- log_test.go
+++ log_test.go
@@ -1,43 +1,12 @@
 package main
 
 import (
-	"io/ioutil"
+	"github.com/rezakamalifard/TestLoggingWithZap/internal/ztest"
 	"testing"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
-
-// A Syncer is a spy for the Sync portion of zapcore.WriteSyncer.
-type Syncer struct {
-	err    error
-	called bool
-}
-
-// SetError sets the error that the Sync method will return.
-func (s *Syncer) SetError(err error) {
-	s.err = err
-}
-
-// Sync records that it was called, then returns the user-supplied error (if
-// any).
-func (s *Syncer) Sync() error {
-	s.called = true
-	return s.err
-}
-
-// Called reports whether the Sync method was called.
-func (s *Syncer) Called() bool {
-	return s.called
-}
-
-// A Discarder sends all writes to ioutil.Discard.
-type Discarder struct{ Syncer }
-
-// Write implements io.Writer.
-func (d *Discarder) Write(b []byte) (int, error) {
-	return ioutil.Discard.Write(b)
-}
 
 func newZapLogger(lvl zapcore.Level) *zap.Logger {
 	ec := zap.NewProductionEncoderConfig()
@@ -46,7 +15,7 @@ func newZapLogger(lvl zapcore.Level) *zap.Logger {
 	enc := zapcore.NewJSONEncoder(ec)
 	return zap.New(zapcore.NewCore(
 		enc,
-		&Discarder{},
+		&ztest.Discarder{},
 		lvl,
 	))
 }
